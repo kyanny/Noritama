2fc4fc9bf732f2f1d6588bd1235b1a7f1f9ac55e
Kensuke Nagae <kyanny@gmail.com>
2011-05-07 01:14:25 +0900

    routes

--- lib/Noritama/Web/Dispatcher.pm
+++ lib/Noritama/Web/Dispatcher.pm
@@ -3,9 +3,29 @@ use strict;
 use warnings;
 use Amon2::Web::Dispatcher::Lite;
 
-any '/' => sub {
+get '/' => sub {
     my ($c) = @_;
     $c->render('index.tt');
+};
+
+get '/api' => sub {
+    my ($c) = @_;
+};
+
+post '/api' => sub {
+    my ($c) = @_;
+};
+
+get '/api/:post_id' => sub {
+    my ($c) = @_;
+};
+
+any [qw/PUT/] => '/api/:post_id' => sub {
+    my ($c) = @_;
+};
+
+any [qw/DELETE/] => '/api/:post_id' => sub {
+    my ($c) = @_;
 };
 
 1;
