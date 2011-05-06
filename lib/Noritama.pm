65bd5a4e9e2e7a2bb7a078d83b737cda49600f64
Kensuke Nagae <kyanny@gmail.com>
2011-05-07 01:50:11 +0900

    $self->teng

--- lib/Noritama.pm
+++ lib/Noritama.pm
@@ -4,7 +4,17 @@ use warnings;
 use parent qw/Amon2/;
 our $VERSION='0.01';
 use 5.008001;
+use Noritama::Model::Post;
 
 # __PACKAGE__->load_plugin(qw/DBI/);
+
+sub teng {
+    my $self = shift;
+    if (!defined $self->{teng}) {
+        my $c = $self->config->{Teng};
+        $self->{teng} = Noritama::Model::Post->new(connect_info => $c);
+    }
+    $self->{teng};
+}
 
 1;
