e11bf1831c4013e936386b4747676f0ec7e1e229
Kensuke Nagae <kyanny@gmail.com>
2011-05-07 01:01:53 +0900

    initial commit

---
+++ lib/Noritama/Web/Dispatcher.pm
@@ -0,0 +1,11 @@
+package Noritama::Web::Dispatcher;
+use strict;
+use warnings;
+use Amon2::Web::Dispatcher::Lite;
+
+any '/' => sub {
+    my ($c) = @_;
+    $c->render('index.tt');
+};
+
+1;
---
+++ t/00_compile.t
@@ -0,0 +1,11 @@
+use strict;
+use warnings;
+use Test::More;
+
+use_ok $_ for qw(
+    Noritama
+    Noritama::Web
+    Noritama::Web::Dispatcher
+);
+
+done_testing;
