396f9d5cb993e31e520e8c7586eca557c39b7630
Kensuke Nagae <kyanny@gmail.com>
2011-05-07 02:40:33 +0900

    method modifiers (trigger)

--- lib/Noritama/Model/Post.pm
+++ lib/Noritama/Model/Post.pm
@@ -2,5 +2,20 @@ package Noritama::Model::Post;
 use strict;
 use warnings;
 use parent 'Teng';
+use Mouse;
+use Data::UUID;
+use Time::Piece::MySQL;
+
+before 'insert' => sub {
+    my ($self, $table_name, $row_data, $prefix) = @_;
+    $row_data->{post_id} = Data::UUID->new->create_str;
+    $row_data->{created_at} = localtime->mysql_datetime;
+    $row_data->{updated_at} = localtime->mysql_datetime;
+};
+
+before 'update' => sub {
+    my ($self, $table_name, $update_row_data, $update_condition) = @_;
+    $update_row_data->{updated_at} = localtime->mysql_datetime;
+};
 
 1;
