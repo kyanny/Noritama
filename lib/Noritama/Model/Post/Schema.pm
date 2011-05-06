3f808f13b907106453fc4e750c13a861095a7990
Kensuke Nagae <kyanny@gmail.com>
2011-05-07 02:41:43 +0900

    inflate/deflate

--- lib/Noritama/Model/Post/Schema.pm
+++ lib/Noritama/Model/Post/Schema.pm
@@ -2,6 +2,7 @@ package Noritama::Model::Post::Schema;
 use strict;
 use warnings;
 use Teng::Schema::Declare;
+use Time::Piece::MySQL;
 
 table {
     name 'posts';
@@ -13,6 +14,34 @@ table {
                   updated_at
                   deleted_at
           );
+
+    inflate 'created_at' => sub {
+        my ($col_value) = @_;
+        return Time::Piece::MySQL->from_mysql_datetime($col_value);
+    };
+    deflate 'created_at' => sub {
+        my ($col_value) = @_;
+        return ref $col_value ? $col_value->mysql_datetime : $col_value;
+    };
+
+    inflate 'updated_at' => sub {
+        my ($col_value) = @_;
+        return Time::Piece::MySQL->from_mysql_datetime($col_value);
+    };
+    deflate 'updated_at' => sub {
+        my ($col_value) = @_;
+        return ref $col_value ? $col_value->mysql_datetime : $col_value;
+    };
+
+    inflate 'deleted_at' => sub {
+        my ($col_value) = @_;
+        return Time::Piece::MySQL->from_mysql_datetime($col_value);
+    };
+    deflate 'deleted_at' => sub {
+        my ($col_value) = @_;
+        return ref $col_value ? $col_value->mysql_datetime : $col_value;
+    };
+
 };
 
 1;
